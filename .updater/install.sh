95920d3cc3e669c259bb287b6ad7c71fda6564e8
Steven de Salas <steven.de-salas@newcrosshealthcare.com>
2023-04-06 23:07:00 +0100

    Fix updater

--- .updater/install.sh
+++ .updater/install.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 (crontab -l 2>/dev/null; echo "*/1 * * * * cd $SCRIPT_DIR && ./check.sh >> check.log 2>&1") | sudo crontab -
-(crontab -l 2>/dev/null; echo "* */1 * * * cd $SCRIPT_DIR && echo \"$(tail -1000 check.log)\" > check.log") | sudo crontab -
+(crontab -l 2>/dev/null; echo "0 * * * * cd $SCRIPT_DIR && tail -1000 check.log | cat > check.log") | sudo crontab -
