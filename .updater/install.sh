fa183b71737a49be0cfa17f86c2f94b791a3fdad
Steven de Salas <steven.de-salas@newcrosshealthcare.com>
2023-04-07 10:13:04 +0100

    Updater

--- .updater/install.sh
+++ .updater/install.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
 #
-# Installs updater using cron
+# Installs updater using cron+git
+# Check git every minute to compare remote with local
 # Logs output to `update.log`
 #
 #
 # 1) Cron doesnt know the script directory so find out where it is
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-# 2) Check git every minute to compare remote with local, log to `update.log`
+# 2) Check git remote with local, log to `update.log`
 (crontab -l 2>/dev/null; echo "*/1 * * * * cd $SCRIPT_DIR && ./check.sh >> update.log 2>&1") | sudo crontab -
-# 3) Every hour, trim the logs to last 1000 lines
+# 3) Keep the logs trimmed to last 1000 lines
 (crontab -l 2>/dev/null; echo "0 * * * * cd $SCRIPT_DIR && tail -1000 update.log | cat > update.log") | sudo crontab -
-
-echo "Cron+git updater installed. Check the `update.log` in a minute or two."
+# Done
+echo "Rocky updater installed. Check the `update.log` in a minute or two."
