3cff8aa10ab57b0382fba827edb9451be5bfa140
Steven de Salas <sdesalas@users.noreply.github.com>
2023-04-25 08:39:53 +0100

    Update user.js

--- server/lib/user.js
+++ server/lib/user.js
@@ -16,7 +16,7 @@ const users = Object.keys(process.env)
 
 export default class User {
   static me(req) {
-    console.log('User.me(%s)', req.headers.authorization);
+    console.log('User.me(%s)');
     if (!req?.headers?.authorization) return null;
     const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
     const [username] = Buffer.from(b64auth, 'base64').toString().split(':');
