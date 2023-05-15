301deabb7ca50921c622971aa291866d2b10ebff
Steven de Salas <steven.de-salas@newcrosshealthcare.com>
2023-05-15 16:45:43 +0100

    Updater

--- .updater/install.sh
+++ .updater/install.sh
@@ -5,12 +5,16 @@
 # Logs output to `update.log`
 #
 #
+echo "ROCKY Minecraft Server Controller Updater"
+echo ""
+echo "This updater script will elevate permissions to 'root' so it can sudo docker calls inside 'check.sh' without a problem."
+echo "If you dont feel comfortable with this feel free to do this some other way."
 # 1) Cron doesnt know the script directory so find out where it is
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # 2) Check git remote with local, log to `update.log`
-(crontab -l 2>/dev/null; echo "*/1 * * * * cd $SCRIPT_DIR && ./check.sh >> update.log 2>&1") | crontab -
+(sudo crontab -l 2>/dev/null; echo "*/1 * * * * cd $SCRIPT_DIR && ./check.sh >> update.log 2>&1") | sudo crontab -
 # 3) Keep the logs trimmed to last 1000 lines
-(crontab -l 2>/dev/null; echo "0 * * * * cd $SCRIPT_DIR && tail -1000 update.log | cat > update.log") | crontab -
+(sudo crontab -l 2>/dev/null; echo "0 * * * * cd $SCRIPT_DIR && tail -1000 update.log | cat > update.log") | sudo crontab -
 # Done
 crontab -l
 echo ""
