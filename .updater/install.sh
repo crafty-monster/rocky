a47dfe6cf6906abed99112ae724ab85ac70a6472
Steven de Salas <steven.de-salas@newcrosshealthcare.com>
2023-05-15 16:46:56 +0100

    Updater

--- .updater/install.sh
+++ .updater/install.sh
@@ -15,7 +15,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 (sudo crontab -l 2>/dev/null; echo "*/1 * * * * cd $SCRIPT_DIR && ./check.sh >> update.log 2>&1") | sudo crontab -
 # 3) Keep the logs trimmed to last 1000 lines
 (sudo crontab -l 2>/dev/null; echo "0 * * * * cd $SCRIPT_DIR && tail -1000 update.log | cat > update.log") | sudo crontab -
-# Done
-crontab -l
+# Done. List the jobs
+sudo crontab -l
 echo ""
 echo "Rocky updater installed. Check the 'update.log' in a minute or two."
