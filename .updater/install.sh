da349e30a096514610a2203f81b6ecf3c995f599
Steven de Salas <steven.de-salas@newcrosshealthcare.com>
2023-05-15 16:39:26 +0100

    Updater

--- .updater/install.sh
+++ .updater/install.sh
@@ -8,9 +8,9 @@
 # 1) Cron doesnt know the script directory so find out where it is
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # 2) Check git remote with local, log to `update.log`
-(crontab -l 2>/dev/null; echo "*/1 * * * * cd $SCRIPT_DIR && ./check.sh >> update.log 2>&1") | sudo crontab -
+(crontab -l 2>/dev/null; echo "*/1 * * * * cd $SCRIPT_DIR && ./check.sh >> update.log 2>&1") | crontab -
 # 3) Keep the logs trimmed to last 1000 lines
-(crontab -l 2>/dev/null; echo "0 * * * * cd $SCRIPT_DIR && tail -1000 update.log | cat > update.log") | sudo crontab -
+(crontab -l 2>/dev/null; echo "0 * * * * cd $SCRIPT_DIR && tail -1000 update.log | cat > update.log") | crontab -
 # Done
 crontab -l
 echo ""
